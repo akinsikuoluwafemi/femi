d8a18846167c9f86ac75f58708c894fba7735530
Femi Akinsiku <oluwafemidavid.akinsiku@publicisgroupe.cz>
2024-03-28 11:54:15 +0000

    First version

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`Femi Akinsiku 
+
+
+As a seasoned Full Stack Developer, I specialize in React, TypeScript, and Node.js technologies. With a track record of delivering innovative user interfaces and robust backend solutions, I excel in crafting seamless experiences from end to end. My passion for staying at the forefront of industry trends and leading cross-functional teams ensures the delivery of cutting-edge solutions that drive user satisfaction and business success.
+
+Twitter: https://twitter.com/david_akinsiku
+Github: https://github.com/akinsikuoluwafemi
+Website: https://femiakin.com/
+
+`);
+
+//npm link
