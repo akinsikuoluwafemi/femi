63b35e2b16bbfc22079653437fad6298ce482ea4
Femi Akinsiku <oluwafemidavid.akinsiku@publicisgroupe.cz>
2024-03-28 12:33:06 +0000

    improvment: Clears console

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
-console.log(`Femi Akinsiku 
+
+const clearConsole = require("clear-any-console");
+
+clearConsole();
+
+console.log(`Femi Akinsiku
 
 
 As a seasoned Full Stack Developer, I specialize in React, TypeScript, and Node.js technologies. With a track record of delivering innovative user interfaces and robust backend solutions, I excel in crafting seamless experiences from end to end. My passion for staying at the forefront of industry trends and leading cross-functional teams ensures the delivery of cutting-edge solutions that drive user satisfaction and business success.
 
-Twitter: https://twitter.com/david_akinsiku
-Github: https://github.com/akinsikuoluwafemi
-Website: https://femiakin.com/
+🔗 Twitter: https://twitter.com/david_akinsiku
+⚓️ Github: https://github.com/akinsikuoluwafemi
+🧑‍💻 Website: https://femiakin.com/
 
 `);
 
